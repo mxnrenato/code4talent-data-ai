54ea34601a8b78e7f6cc88bcf2086aca46bf157f
Renato Maximiliano Rivera Abad <61995338+mxnrenato@users.noreply.github.com>
2025-10-24 13:53:33 -0500

    feat: integrate redis publish

--- NaN_Exception/src/producer/main_producer.py
+++ NaN_Exception/src/producer/main_producer.py
@@ -1,12 +1,24 @@
 from api_call import data_call
-import logging
+import redis
+import json
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_CHANNEL = "weather_channel"
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 def main():
-    logging.info("Starting API call loop...")
     while True:
-        appended_columns = data_call()
-        
+        data = data_call()
+        if data:
+            r.publish(REDIS_CHANNEL, json.dumps(data))
+            print("Mensaje publicado en Redis.")
         time.sleep(15)
 
 if __name__ == "__main__":
