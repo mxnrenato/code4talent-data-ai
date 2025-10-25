a8358e282822662d1ba77f07d56cd628e6624be6
Renato Maximiliano Rivera Abad <61995338+mxnrenato@users.noreply.github.com>
2025-10-24 19:20:25 -0500

    feat: implement logs in api_call

--- NaN_Exception/src/producer/api_call.py
+++ NaN_Exception/src/producer/api_call.py
@@ -1,16 +1,19 @@
 import requests
 import logging
 from datetime import datetime
+from log_util.logger_config import setup_logger
 
 url = "https://api.open-meteo.com/v1/forecast?latitude=-0.2298&longitude=-78.525&current=temperature_2m,relative_humidity_2m,wind_speed_10m,cloud_cover&timezone=America%2FNew_York&forecast_days=1"
 
-logging.basicConfig(level=logging.INFO)
+logger = setup_logger(__name__, "api_call.log")
 
 def data_call():
     try:
+        logger.info("Request a Open Meteor...")
         response = requests.get(url)
         data = response.json()
-
+        
+        logger.info("Obteniendo información lat y lon...")
         information_column = {
             'latitude': data['latitude'],
             'longitude': data['longitude']
@@ -22,7 +25,7 @@ def data_call():
             'wind_speed_10m': data['current']['wind_speed_10m'],
             'cloud_cover': data['current']['cloud_cover']
         }
-
+        logger.info("Generación de Timestamp...")
         appended_columns = {
             **information_column,
             **values_column,
